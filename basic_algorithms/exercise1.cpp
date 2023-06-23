211ed50e9b8edaedec098f424cb8ef64f4f5f825
ifituburyo <izerifitepaci98@gmail.com>
2023-06-23 19:28:19 +0200

    exerices number one on basic algorithm

--- basic_algorithms/exercise1.cpp
+++ basic_algorithms/exercise1.cpp
@@ -1,8 +1,37 @@
 #include<iostream>
 using namespace std;
+int calculate(int number1, int number2);
 
 int main(){
-    cout<<"Hello world";
-    cout<<"'hello my friend , how are you doing here'";
+
+    int num1;
+    int num2;
+    int result;
+
+    cout<<"Enter number1 "<<endl;
+    cin>>num1;
+    cout<<"enter the number 2" <<endl;
+
+    cin>>num2;
+    result = calculate(num1,num2);
+    cout<<"we can calculate the sam now if the number are diffrent and triple of sum if number are the same "<< result;
+    
+
     return 0;
+}
+
+int calculate (int number1, int number2)
+{
+    int sum;
+    if(number1!=number2)
+    {
+    sum=number1+number2; return sum; 
+    }
+    else 
+    {
+        int triple =3*(number1+number2);
+        return triple;
+    }
+
+   
 }
